94c954df919eca0ee9f689c6006a622ee20294ba
liu-jianhao <jianhaoliu17@email.com>
2018-09-21 21:11:34 +0800

    词典操作

---
+++ Chapter02/2.14/checkword.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+word=$1
+grep "^$1$" /usr/share/dict/british-english -q
+if [ $? -eq 0 ]; then
+    echo $word is a dictionary word;
+else
+    echo $word is not a dictionary word;
+fi
